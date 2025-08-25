8d1439408656979b452bde56c169866b3140cf9a
rx342 <15484682+rx342@users.noreply.github.com>
2025-08-25 19:52:20 +0200

    feat(nix): init

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{
+  system ? builtins.currentSystem,
+  source ? import ./npins,
+  pkgs ? import source.nixpkgs {
+    overlays = [ ];
+    config = { };
+    inherit system;
+  },
+}:
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    nushell
+    uv
+  ];
+}
