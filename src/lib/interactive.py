3906a8c0bfcce3f07d9cfa4d3046487b32995f66
rx342 <15484682+rx342@users.noreply.github.com>
2025-09-14 09:07:07 +0200

    feat(interactive): init

---
+++ src/lib/interactive.py
@@ -0,0 +1,264 @@
+import logging
+import os
+from configparser import ConfigParser
+
+import questionary
+
+logger = logging.getLogger(__name__)
+
+
+def ask_config_path() -> str:
+    """Ask the user where do they want to store their configuration file.
+    If the parent directory does not exist, we create it.
+
+    Returns
+    -------
+    str
+        Path to the configuration file
+
+    """
+
+    cfg_path = questionary.text(
+        "Where do you want to store the configuration file?",
+        default="~/.config/yves/config",
+    ).ask()
+    logger.debug(f"Configuration path: {cfg_path}")
+
+    dir_cfg_path = os.path.expanduser(os.path.dirname(cfg_path))
+    if not os.path.exists(dir_cfg_path):
+        logger.debug(f"{dir_cfg_path} does not exist, creating it...")
+        os.makedirs(dir_cfg_path, exist_ok=True)
+
+    return cfg_path
+
+
+def ask_and_update_fs_enable(cfg: ConfigParser) -> bool:
+    """Ask the user if they want to enable file system watch and update `cfg`.
+
+    Parameters
+    ----------
+    cfg : ConfigParser
+        Configuration instance to update
+
+    Returns
+    -------
+    bool
+        User answer
+
+    """
+
+    enable = questionary.confirm(
+        "Would you want to enable the watcher on the filesystem?"
+    ).ask()
+
+    if enable:
+        logger.debug("Enable filesystem watcher")
+        cfg["filesystem"]["enable"] = "True"
+    else:
+        logger.debug("Disable filesystem watcher")
+        cfg["filesystem"]["enable"] = "False"
+
+    return enable
+
+
+def ask_and_update_fs_dirs(cfg: ConfigParser) -> None:
+    """Ask the user which directories they want to watch and update `cfg`.
+
+    Parameters
+    ----------
+    cfg : ConfigParser
+        Configuration instance to update
+
+    """
+
+    str_fs_dirs = ""
+    while True:
+        d = questionary.path(
+            "Which directory do you want to watch (or leave empty to stop loop)?",
+            only_directories=True,
+        ).ask()
+
+        if not d:
+            break
+
+        if str_fs_dirs:
+            str_fs_dirs += f", {d}"
+        else:
+            str_fs_dirs = d
+
+    logger.debug(f"Directories to watch on the file system: {str_fs_dirs}")
+    cfg["filesystem"]["dirs"] = str_fs_dirs
+
+
+def ask_and_update_fs_exclude(cfg: ConfigParser) -> None:
+    """Ask they user what elements they want to exclude from the file system search and update `cfg`.
+
+    Parameters
+    ----------
+    cfg : ConfigParser
+        Configuration instance to update
+
+    """
+
+    exclude_filetypes = questionary.checkbox(
+        "I suggest you to exclude some filetypes/directories to search faster, please select them:",
+        choices=[
+            questionary.Choice("*.pyc", checked=True),
+            questionary.Choice("*.pyo", checked=True),
+            questionary.Choice("*.pyd", checked=True),
+            questionary.Choice("*.swp", checked=True),
+            questionary.Choice("*.swo", checked=True),
+            questionary.Choice("*~", checked=True),
+            questionary.Choice(".git", checked=True),
+        ],
+    ).ask()
+    str_exclude_filetypes = ", ".join([e.lstrip("*") for e in exclude_filetypes])
+
+    logger.debug(f"Element to exclude: {str_exclude_filetypes}")
+    cfg["filesystem"]["exclude_filetypes"] = str_exclude_filetypes
+
+
+def ask_and_update_tmux_enable(cfg: ConfigParser) -> bool:
+    """Ask the user if they want to enable tmux panes watch and update `cfg`.
+
+    Parameters
+    ----------
+    cfg : ConfigParser
+        Configuration instance to update
+
+    Returns
+    -------
+    bool
+        User answer
+
+    """
+
+    enable = questionary.confirm(
+        "Would you want to enable the watcher on tmux panes?"
+    ).ask()
+
+    if enable:
+        logger.debug("Enable tmux watcher")
+        cfg["tmux"]["enable"] = "True"
+    else:
+        logger.debug("Disable tmux watcher")
+        cfg["tmux"]["enable"] = "False"
+
+    return enable
+
+
+def ask_and_update_llm_provider(cfg: ConfigParser) -> None:
+    """Ask the user about the LLM provider they want to use and update `cfg`.
+
+    Parameters
+    ----------
+    cfg : ConfigParser
+        Configuration instance to update
+
+    """
+
+    provider = questionary.text(
+        "Which LLM provider do you want to use (refer to https://docs.litellm.ai/docs/providers)?",
+        default="openai",
+    ).ask()
+    logger.debug(f"LLM Provider: {provider}")
+
+    model_name = questionary.text(
+        "Which model do you want to use (refer to https://docs.litellm.ai/docs/providers)?"
+    ).ask()
+    logger.debug(f"LLM model: {model_name}")
+
+    api_key = questionary.password(
+        "What is your API key (leave empty if your LLM provider does not need it)?"
+    ).ask()
+    logger.debug(f"API key for LLM provider: {api_key}")
+
+    cfg["llm"]["api_key"] = api_key
+    cfg["llm"]["model_name"] = model_name
+    cfg["llm"]["provider"] = provider
+
+
+def is_valid_hour(hour: str) -> bool:
+    """Test if the string `hour` follows the correct date format
+
+    Parameters
+    ----------
+    hour : str
+        String to test
+
+    Returns
+    -------
+    bool
+        Returns `True` if format is correct, else `False`
+
+    """
+
+    from datetime import datetime
+
+    try:
+        datetime.strptime(hour, "%H:%M")
+        return True
+    except ValueError:
+        return False
+
+
+def ask_and_update_summarizer(cfg: ConfigParser) -> None:
+    """Ask the user how they want to get the summary
+
+    Parameters
+    ----------
+    cfg : ConfigParser
+        Configure instance to update
+
+    """
+
+    summary_path = questionary.text(
+        "Where do you want to store every summaries (give a directory)?",
+        default="~/.local/share/yves",
+    ).ask()
+    logger.debug(f"Summary output directory: {summary_path}")
+
+    expand_summary_path = os.path.expanduser(summary_path)
+    if not os.path.exists(expand_summary_path):
+        logger.debug(f"{summary_path} does not exist, creating it...")
+        os.makedirs(expand_summary_path, exist_ok=True)
+
+    summary_hour = questionary.text(
+        'When do you want me to write the report (format: "%H:%m")?',
+        default="19:00",
+        validate=is_valid_hour,
+    ).ask()
+    logger.debug(f"Summary hour: {summary_hour}")
+
+    cfg["summarizer"]["output_dir"] = summary_path
+    cfg["summarizer"]["at"] = summary_hour
+
+
+def configure_interactively() -> None:
+    """Function to interactively configure Yves with the user"""
+
+    from textwrap import dedent
+
+    from lib.cfg import default_config, write_config
+
+    cfg = default_config()
+
+    print(
+        dedent("""\
+        Hello, my name is Yves.
+
+        I am going to ask you some questions to understand how you want me to summarize your day.
+    """)
+    )
+
+    cfg_path = ask_config_path()
+    fs_enable = ask_and_update_fs_enable(cfg)
+    if fs_enable:
+        ask_and_update_fs_dirs(cfg)
+        ask_and_update_fs_exclude(cfg)
+
+    ask_and_update_tmux_enable(cfg)
+    ask_and_update_llm_provider(cfg)
+    ask_and_update_summarizer(cfg)
+
+    write_config(cfg, cfg_path)
