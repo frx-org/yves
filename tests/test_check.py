c855dbedd0b35429486546a4788ce423aa47a041
rx342 <15484682+rx342@users.noreply.github.com>
2025-10-26 14:24:29 +0100

    test(check): init

---
+++ tests/test_check.py
@@ -0,0 +1,44 @@
+"""Test lib/check.py."""
+
+
+def test_check_config(tmp_path):
+    """Test `check_config` if it correctly checks configuration file values."""
+    from uuid import uuid4
+
+    from lib.cfg import default_config, write_config
+    from lib.check import check_config
+
+    abs_path = tmp_path / f"{uuid4().hex}"
+    default_cfg = default_config()
+    write_config(default_cfg, abs_path)
+    assert check_config(abs_path)
+
+    abs_path = tmp_path / f"{uuid4().hex}"
+    default_cfg = default_config()
+    default_cfg["filesystem"]["min_lines_changed"] = "-1"
+    write_config(default_cfg, abs_path)
+    assert not check_config(abs_path)
+
+    abs_path = tmp_path / f"{uuid4().hex}"
+    default_cfg = default_config()
+    default_cfg["filesystem"]["similarity_threshold"] = "1"
+    write_config(default_cfg, abs_path)
+    assert check_config(abs_path)
+
+    abs_path = tmp_path / f"{uuid4().hex}"
+    default_cfg = default_config()
+    default_cfg["filesystem"]["similarity_threshold"] = "0"
+    write_config(default_cfg, abs_path)
+    assert check_config(abs_path)
+
+    abs_path = tmp_path / f"{uuid4().hex}"
+    default_cfg = default_config()
+    default_cfg["filesystem"]["similarity_threshold"] = "-0.01"
+    write_config(default_cfg, abs_path)
+    assert not check_config(abs_path)
+
+    abs_path = tmp_path / f"{uuid4().hex}"
+    default_cfg = default_config()
+    default_cfg["filesystem"]["similarity_threshold"] = "1.2"
+    write_config(default_cfg, abs_path)
+    assert not check_config(abs_path)
